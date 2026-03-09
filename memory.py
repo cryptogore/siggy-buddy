d63120fa8dcceee319a2ed5f02eddefc4ddf5bc7
cryptogore <141570530+cryptogore@users.noreply.github.com>
2026-03-10 02:32:56 +0700

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,16 +1,16 @@
 conversation_memory = {}
 
-def save_message(user_id, message, reply):
+def save_message(user_id,user,reply):
 
     if user_id not in conversation_memory:
-        conversation_memory[user_id] = []
+        conversation_memory[user_id]=[]
 
     conversation_memory[user_id].append({
-        "user": message,
-        "assistant": reply
+        "user":user,
+        "assistant":reply
     })
 
 
 def get_history(user_id):
 
-    return conversation_memory.get(user_id, [])
+    return conversation_memory.get(user_id,[])
